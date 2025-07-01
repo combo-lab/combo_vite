37e68026f302ca736976dfc1334115b3768ae1c8
Zeke Dou <zeke@zekedou.live>
2025-07-01 21:49:13 +0800

    init: mix new combo_vite --app combo_vite --module Combo.Vite

---
+++ lib/combo/vite.ex
@@ -0,0 +1,18 @@
+defmodule Combo.Vite do
+  @moduledoc """
+  Documentation for `Combo.Vite`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Combo.Vite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/combo/vite_test.exs
@@ -0,0 +1,8 @@
+defmodule Combo.ViteTest do
+  use ExUnit.Case
+  doctest Combo.Vite
+
+  test "greets the world" do
+    assert Combo.Vite.hello() == :world
+  end
+end
