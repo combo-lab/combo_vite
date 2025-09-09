9cbdfbdad518807e415d0a9fbf00e475cc1f2009
Zeke Dou <zeke@zekedou.live>
2025-09-09 16:03:31 +0800

    refactor: polish components

--- lib/combo/vite/components.ex
+++ lib/combo/vite/components.ex
@@ -4,55 +4,69 @@ defmodule Combo.Vite.Components do
 
   ## Usage
 
-  Put the following into your `<head />` of your root layout:
+  Add following code into the html_helpers function of your endpoint:
 
-      <Combo.Vite.Components.assets
-        names={["js/app.js", "css/app.css"]}
-        manifest={{:my_app, "priv/static/.vite/manifest.js"}}
-        dev_server={Combo.Vite.Components.has_vite_watcher?(MyAppWeb.Endpoint)}
-      />
+      defmodule Demo.Web do
+        # ...
 
-  If you want to make use of the dev server you need to provide the `to_url` option.
+        defp html_helpers do
+          quote do
+            # ...
 
-      <Combo.Vite.Components.assets
-        names={["js/app.js", "css/app.css"]}
-        manifest={{:my_app, "priv/static/.vite/manifest.js"}}
-        dev_server={Combo.Vite.Components.has_vite_watcher?(MyAppWeb.Endpoint)}
-        to_url={fn p -> static_url(@conn, p) end}
-      />
+            use Combo.Vite.Components,
+              endpoint: Demo.Web.Endpoint,
+              static_dir: {:demo, "priv/static"},
+              ssr_out_dir: {:demo, "priv/ssr"}
 
-  This also requires having the static_url configured for the endpoint.
+            # ...
+          end
+        end
 
-      config :myapp, MyAppWeb.Endpoint,
-        static_url: [host: "localhost", port: 5173]
+        # ...
+      end
 
+  Then, all the components and helper functions will be available in your
+  inline templates or template files.
 
+  ## References
 
-
-    * https://vite.dev/guide/backend-integration.html
+    * [Vite - Guide - Backend Integration](https://vite.dev/guide/backend-integration.html)
 
   """
   use Combo.HTML
   alias Combo.Vite.Manifest
 
   defmacro __using__(opts) do
+    # opts =
+    #   if Keyword.keyword?(opts) do
+    #     for {k, v} <- opts do
+    #       if Macro.quoted_literal?(v) do
+    #         {k, Macro.prewalk(v, &expand_alias(&1, __CALLER__))}
+    #       else
+    #         {k, v}
+    #       end
+    #     end
+    #   else
+    #     opts
+    #   end
+
     config_ast = build_config_ast(opts)
 
     quote do
       @combo_vite_config unquote(Macro.expand(config_ast, __CALLER__))
 
-      attr :entries, :list, required: true
+      attr :names, :list, required: true
 
-      def vite(assigns) do
+      def vite_assets(assigns) do
         assigns = assign(assigns, :config, combo_vite_config())
-        unquote(__MODULE__).vite(assigns)
+        unquote(__MODULE__).vite_assets(assigns)
       end
 
-      attr :entry, :string, required: true
+      attr :name, :string, required: true
 
-      def vite_content(assigns) do
+      def vite_asset(assigns) do
         assigns = assign(assigns, :config, combo_vite_config())
-        unquote(__MODULE__).vite_content(assigns)
+        unquote(__MODULE__).vite_asset(assigns)
       end
 
       attr :rest, :global
@@ -62,8 +76,12 @@ defmodule Combo.Vite.Components do
         unquote(__MODULE__).vite_react_refresh(assigns)
       end
 
-      def vite_url(key) do
-        unquote(__MODULE__).vite_url(key, combo_vite_config())
+      def vite_url(name) do
+        unquote(__MODULE__).vite_url(name, combo_vite_config())
+      end
+
+      def vite_content(name) do
+        unquote(__MODULE__).vite_url(name, combo_vite_config())
       end
 
       defp combo_vite_config, do: @combo_vite_config
@@ -113,14 +131,27 @@ defmodule Combo.Vite.Components do
   end
 
   @doc """
+  Renders elements for given assets.
 
+  This component auto-detects the mode of Vite and behaves accordingly:
+
+    * In dev mode, it loads the `@vite/client` (for Hot Module Replacement)
+      and the given assets.
+    * In build mode, it loads the compiled and versioned assets, including any
+      imported CSS.
+
+  ## Examples
+
+  ```ceex
+  <.vite_assets names={["src/css/app.css", "src/js/app.js"]} />
+  ```
   """
-  attr :entries, :list, required: true
+  attr :names, :string, required: true
   attr :config, :map, required: true
 
-  def vite(%{entries: entries, config: config} = assigns) do
-    entries = Enum.map(entries, &remove_leading_slash/1)
-    assigns = assign(assigns, :entries, entries)
+  def vite_assets(%{names: names, config: config} = assigns) do
+    names = Enum.map(names, &remove_leading_slash/1)
+    assigns = assign(assigns, :names, names)
 
     if running_hot?(config),
       do: vite_on_dev_server(assigns),
@@ -128,34 +159,33 @@ defmodule Combo.Vite.Components do
   end
 
   @doc """
-  Bug: Heex's <script> and <style> can't interpret the components inside them.
+  Renders an element for a given asset.
+
+  Different from `vite_assets/1`, it dosen't loads the `@vite/client` in dev
+  mode. All other behaviors are the same.
+
+  ## Examples
+
+  ```ceex
+  <.vite_asset name="src/css/app.css" />
+  ```
   """
-  attr :entry, :string, required: true
+  attr :name, :string, required: true
   attr :config, :map, required: true
 
-  def vite_content(%{entry: entry, config: config} = assigns) do
-    entry = remove_leading_slash(entry)
-    manifest = config.manifest_file |> File.read!() |> Manifest.parse()
-    # throw error if file not found
+  def vite_asset(%{name: name, config: config} = assigns) do
+    name = remove_leading_slash(name)
+    assigns = assign(assigns, :name, name)
 
-    chunk = Manifest.fetch_chunk!(manifest, entry)
-    # throw error if entry not found
-
-    file = Path.join(config.out_dir, chunk.file)
-    # throw error if file not found
-    content = File.read!(file)
-
-    assigns = assigns |> assign(:entry, entry) |> assign(:content, content)
-
-    ~CE"""
-    {{:safe, @content}}
-    """
+    if running_hot?(config),
+      do: vite_on_dev_server(assigns),
+      else: vite_on_manifest(assigns)
   end
 
   @doc """
-  Generates script for React refresh runtime.
+  Renders script element for React refresh runtime.
 
-  Note that the script is generated only when the dev server is running.
+  > Note that the script is generated only when the dev server is running.
   """
   attr :config, :map, required: true
   attr :rest, :global
@@ -164,7 +194,7 @@ defmodule Combo.Vite.Components do
     ~CE"""
     <%= if running_hot?(@config) do %>
       <script type="module" {@rest}>
-        import RefreshRuntime from "<%= to_dev_server_url(@config, "@react-refresh") %>";
+        import RefreshRuntime from "<%= to_dev_server_url("@react-refresh", @config) %>";
         RefreshRuntime.injectIntoGlobalHook(window);
         window.$RefreshReg$ = () => {};
         window.$RefreshSig$ = () => (type) => type;
@@ -175,55 +205,105 @@ defmodule Combo.Vite.Components do
   end
 
   @doc """
-  Gets the URL of a chunk.
+  Gets the URL of a given asset.
+
+  ## Examples
+
+  ```ceex
+  <img src={vite_url("src/images/logo.png")} />
+  ```
   """
-  def vite_url(key, config) do
+  def vite_url(name, config) do
     if running_hot?(config) do
-      to_dev_server_url(config, key)
+      to_dev_server_url(name, config)
     else
       manifest = config.manifest_file |> File.read!() |> Manifest.parse()
-      chunk = Manifest.fetch_chunk!(manifest, key)
-      to_static_url(config, chunk.file)
+      chunk = Manifest.fetch_chunk!(manifest, name)
+      to_static_url(chunk.file, config)
     end
   end
 
-  defp remove_leading_slash(path), do: Path.relative(path)
+  @doc """
+  Gets the content of a given asset.
 
-  defp running_hot?(config), do: File.exists?(config.hot_file)
+  ## Examples
 
-  defp vite_on_dev_server(%{entries: entries, config: _config} = assigns) do
-    assigns = assign(assigns, :entries, ["@vite/client" | entries])
+  ```ceex
+  <style>
+    {raw vite_content("src/css/app.css")}
+  </style>
+  <script>
+    {raw vite_content("src/js/app.js")}
+  </script>
+  ```
+  """
+  def vite_content(name, config) do
+    name = remove_leading_slash(name)
+
+    manifest = config.manifest_file |> File.read!() |> Manifest.parse()
+    # throw error if file not found
+
+    chunk = Manifest.fetch_chunk!(manifest, name)
+    # throw error if chunk not found
+
+    file = Path.join(config.out_dir, chunk.file)
+    # throw error if file not found
+
+    File.read!(file)
+  end
+
+  ## For development mode
+
+  defp vite_on_dev_server(%{names: names, config: _config} = assigns) do
+    assigns = assign(assigns, :names, ["@vite/client" | names])
 
     ~CE"""
-    <%= for entry <- @entries do %>
-      <.tag file={entry} to_url={&to_dev_server_url/2} config={@config} />
+    <%= for name <- @names do %>
+      <.tag file={name} to_url={&to_dev_server_url/2} config={@config} />
     <% end %>
     """
   end
 
-  defp to_dev_server_url(config, entry) do
-    base_url = config.hot_file |> File.read!() |> String.trim()
-    Path.join([base_url, entry])
+  defp vite_on_dev_server(%{name: _name, config: _config} = assigns) do
+    ~CE"""
+    <.tag file={@name} to_url={&to_dev_server_url/2} config={@config} />
+    """
   end
 
-  defp vite_on_manifest(%{entries: _entries, config: config} = assigns) do
+  defp to_dev_server_url(name, config) do
+    base_url = config.hot_file |> File.read!() |> String.trim()
+    Path.join([base_url, name])
+  end
+
+  ## For production mode
+
+  defp vite_on_manifest(%{names: _names, config: config} = assigns) do
     manifest = config.manifest_file |> File.read!() |> Manifest.parse()
     assigns = assign(assigns, :manifest, manifest)
 
     ~CE"""
-    <%= for entry <- @entries do %>
-      <.entry_tags entry={entry} manifest={@manifest} config={@config} />
+    <%= for name <- @names do %>
+      <.asset_tags name={name} manifest={@manifest} config={@config} />
     <% end %>
     """
   end
 
-  defp entry_tags(%{entry: entry, manifest: manifest, config: config} = assigns) do
+  defp vite_on_manifest(%{name: _name, config: config} = assigns) do
+    manifest = config.manifest_file |> File.read!() |> Manifest.parse()
+    assigns = assign(assigns, :manifest, manifest)
+
+    ~CE"""
+    <.asset_tags name={@name} manifest={@manifest} config={@config} />
+    """
+  end
+
+  defp asset_tags(%{name: name, manifest: manifest, config: _config} = assigns) do
     assigns =
-      assign(assigns,
-        chunk: Manifest.fetch_chunk!(manifest, entry),
-        imported_chunks: Manifest.fetch_imported_chunks!(manifest, entry),
-        config: config
-      )
+      assigns
+      |> assign(:name, nil)
+      |> assign(:manifest, nil)
+      |> assign(:chunk, Manifest.fetch_chunk!(manifest, name))
+      |> assign(:imported_chunks, Manifest.fetch_imported_chunks!(manifest, name))
 
     ~CE"""
     <%= for css <- @chunk.css do %>
@@ -241,7 +321,7 @@ defmodule Combo.Vite.Components do
     """
   end
 
-  defp to_static_url(config, entry) do
+  defp to_static_url(file, config) do
     static_url_config = config.endpoint.config(:static_url)
 
     base_url =
@@ -249,8 +329,14 @@ defmodule Combo.Vite.Components do
         do: struct(URI, static_url_config) |> URI.to_string(),
         else: "/"
 
-    Path.join([base_url, config.build_dir, entry])
+    Path.join([base_url, config.build_dir, file])
   end
+
+  ## Helpers
+
+  defp remove_leading_slash(name), do: Path.relative(name)
+
+  defp running_hot?(config), do: File.exists?(config.hot_file)
 
   attr :file, :string, required: true
   attr :to_url, {:fun, 2}, required: true
@@ -260,16 +346,16 @@ defmodule Combo.Vite.Components do
   defp tag(assigns) do
     ~CE"""
     <%= if is_css(@file) do %>
-      <link rel="stylesheet" href={apply(@to_url, [@config, @file])} {@rest} />
+      <link rel="stylesheet" href={apply(@to_url, [@file, @config])} {@rest} />
     <% else %>
-      <script type="module" src={apply(@to_url, [@config, @file])} {@rest}>
+      <script type="module" src={apply(@to_url, [@file, @config])} {@rest}>
       </script>
     <% end %>
     """
   end
 
-  defp is_css(entry) do
-    Regex.match?(~r/\.(css|less|sass|scss|styl|stylus|pcss|postcss)(\?[^\.]*)?$/, entry)
+  defp is_css(name) do
+    Regex.match?(~r/\.(css|less|sass|scss|styl|stylus|pcss|postcss)(\?[^\.]*)?$/, name)
   end
 
   # defp cache_manifest(file) do
