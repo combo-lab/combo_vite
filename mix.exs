6454b98a3bf09ecedafbed7df5444f8aade11fa5
Zeke Dou <zeke@zekedou.live>
2025-10-04 23:06:11 +0800

    chore: polish mix alias - tag

--- mix.exs
+++ mix.exs
@@ -90,7 +90,7 @@ defmodule Combo.Vite.MixProject do
 
   defp tag_release(_) do
     Mix.shell().info("Tagging release as v#{@version}")
-    System.cmd("git", ["tag", "v#{@version}"])
+    System.cmd("git", ["tag", "v#{@version}", "--message", "Release v#{@version}"])
     System.cmd("git", ["push", "--tags"])
   end
 end
